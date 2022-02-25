b738df7d00609ffc9a7f1b76d86a3ef5f1b7786f
aubreygilman <97066832+aubreygilman@users.noreply.github.com>
2022-02-24 21:37:05 -0800

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -1,7 +1,9 @@
 from turtle import TurtleScreen
 import pygame
+import button
 
 pygame.init()
+pygame.font.init()
 
 # setting up the window
 width = 600
@@ -12,7 +14,11 @@ pygame.display.set_caption('VORDLE')
 # window colors
 black = (0,0,0)
 white = (255,255,255)
-gray = (211,211,211)
+gray = (150,150,150)
+
+# load button image
+stats_img = pygame.image.load('stats.png').convert_alpha()
+stats_button = button.Button(555, 10, stats_img, 0.5)
 
 # initializa game board boxes
 turn = 0
@@ -43,14 +49,14 @@ def draw_boxes():
     global boxes
     for col in range(0,5):
         for row in range(0,6):
-            pygame.draw.rect(screen, white, [col * 100 + 65, row * 100 + 60, 70, 70], 3, 5)
+            pygame.draw.rect(screen, white, [col * 100 + 65, row * 100 + 60, 70, 70], 3)
 
 # draw keyboard
 def draw_keys1():
     global turn 
     global keys1
     for col in range(0,10):
-        pygame.draw.rect(screen, gray, [col * 40 + 100, 650, 30, 40], 3, 5)
+        pygame.draw.rect(screen, gray, [col * 40 + 100, 650, 30, 40], border_radius=7)
         keys_text = game_font.render(keys1[col], True, white)
         screen.blit(keys_text, (col * 40 + 110, 662))
 
@@ -58,7 +64,7 @@ def draw_keys2():
     global turn 
     global keys2
     for col in range(0,9):
-        pygame.draw.rect(screen, gray, [col * 40 + 120, 700, 30, 40], 3, 5)
+        pygame.draw.rect(screen, gray, [col * 40 + 120, 700, 30, 40], border_radius=7)
         keys_text = game_font.render(keys2[col], True, white)
         screen.blit(keys_text, (col * 40 + 132, 711))
 
@@ -66,22 +72,70 @@ def draw_keys3():
     global turn 
     global keys3
     for col in range(0,7):
-        pygame.draw.rect(screen, gray, [col * 40 + 160, 750, 30, 40], 3, 5)
+        pygame.draw.rect(screen, gray, [col * 40 + 160, 750, 30, 40], border_radius=7)
         keys_text = game_font.render(keys3[col], True, white)
         screen.blit(keys_text, (col * 40 + 170, 763))
-# game loop
-running = True
-while running:
-    timer.tick(fps)
-    screen.fill(black)
-    screen.blit(title, titleRect)
-    draw_boxes()
-    draw_keys1()
-    draw_keys2()
-    draw_keys3()
-    for event in pygame.event.get():
-        if  event.type == pygame.QUIT:
-            running = False
 
-    pygame.display.flip()
-pygame.quit()
+# stats button loop
+def stats():
+    width = 500
+    height = 400
+    screen2 = pygame.display.set_mode([width, height])
+    pygame.display.set_caption('STATISTICS')
+
+    stats_font = pygame.font.Font('freesansbold.ttf', 20)
+    stats_title = stats_font.render('STATISTICS', True, white, black)
+    titleRect2 = stats_title.get_rect()
+    titleRect2.center = (width // 2, height - 330)
+
+    hist_title = stats_font.render('GUESS DISTRIBUTION', True, white, black)
+    titleRect3 = hist_title.get_rect()
+    titleRect3.center = (width // 2, height - 200)
+
+    exit_img = pygame.image.load('exit.png').convert_alpha()
+    exit_button = button.Button(470, 10, exit_img, 0.5)
+
+    running = True
+    while running:
+        timer.tick(fps)
+        screen.fill(black)
+        screen2.blit(stats_title, titleRect2)
+        screen2.blit(hist_title, titleRect3)
+
+        if exit_button.draw(screen2):
+            game()
+
+        # exit game loop
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        pygame.display.flip()
+    pygame.quit()
+
+# game loop
+def game():
+    running = True
+    while running:
+        timer.tick(fps)
+        screen.fill(black)
+        screen.blit(title, titleRect)
+
+        # if stats button is pressed
+        if stats_button.draw(screen):
+            stats()
+
+        # draw game board
+        draw_boxes()
+        draw_keys1()
+        draw_keys2()
+        draw_keys3()
+
+        # exit game loop
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        pygame.display.flip()
+    pygame.quit()
+game()
